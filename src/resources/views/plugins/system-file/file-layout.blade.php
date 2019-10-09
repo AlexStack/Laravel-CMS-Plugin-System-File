3cc63728642b35b29a39d257712055fff0a628c1
Alex <alex@10168.com>
2019-10-09 19:16:24 +1300

    Add more shortcuts

--- src/resources/views/plugins/system-file/file-layout.blade.php
+++ src/resources/views/plugins/system-file/file-layout.blade.php
@@ -35,19 +35,8 @@
 
         @endif
 
-        <div class="col-md-12 mt-2 text-secondary shortcuts">
-            <img src="/laravel-cms/plugins/system-file/images/favicon-32x32.png" width="16"
-                class="align-top shortcut-logo" />
-            {{$helper->t('plugin-system-file.cms_shortcuts')}} :
-            <a href="?path=resources/views/vendor/laravel-cms" class="btn btn-outline-secondary btn-sm">Templates</a>
-            <a href="?path=public/laravel-cms" class="btn btn-outline-secondary btn-sm">Assets</a>
-            <a href="?path=app/LaravelCms" class="btn btn-outline-secondary btn-sm">Plugin Controllers</a>
+        @include($helper->bladePath('system-file.file-shortcut','plugins'))
 
-            <a href="?path=resources/lang/vendor/laravel-cms" class="btn btn-outline-secondary btn-sm">Lang</a>
-
-            <a href="?path=storage/app/laravel-cms/backups" class="btn btn-outline-secondary btn-sm">Backups</a>
-
-        </div>
     </div>
 </div>
 
---
+++ src/resources/views/plugins/system-file/file-shortcut.blade.php
@@ -0,0 +1,17 @@
+<div class="col-md-12 mt-2 text-secondary shortcuts">
+    <img src="/laravel-cms/plugins/system-file/images/favicon-32x32.png" width="16" class="align-top shortcut-logo" />
+    {{$helper->t('plugin-system-file.cms_shortcuts')}} :
+    <a href="?path=resources/views/vendor/laravel-cms" class="btn btn-outline-secondary btn-sm">Templates</a>
+    <a href="?path=public/laravel-cms" class="btn btn-outline-secondary btn-sm">Assets</a>
+    <a href="?path=app/LaravelCms" class="btn btn-outline-secondary btn-sm">Plugin Controllers</a>
+
+    <a href="?path=resources/lang/vendor/laravel-cms" class="btn btn-outline-secondary btn-sm">Lang</a>
+
+    <a href="?path=storage/app/laravel-cms/backups" class="btn btn-outline-secondary btn-sm">Backups</a>
+
+
+    <a href="?path=config&file=laravel-cms.php" class="btn btn-outline-secondary btn-sm">CMS Config</a>
+    <a href="?path=&file=.env" class="btn btn-outline-secondary btn-sm">ENV</a>
+    <a href="?path=routes&file=web.php" class="btn btn-outline-secondary btn-sm">Web Routes</a>
+
+</div>
